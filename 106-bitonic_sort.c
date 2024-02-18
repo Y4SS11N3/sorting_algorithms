02df68c0c25bce0cd137612f2247c4e33a11c864
YASSINE <contact@yassine.fun>
2024-02-18 12:53:29 -0800

    bitonic sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -26,20 +26,20 @@ void swap(int *a, int *b, int *array, size_t size)
  * @cnt: Number of elements to be sorted.
  * @dir: The direction to sort in.
  */
-void bitonic_merge(int *array, size_t size, int low, int cnt, int dir)
+void bitonic_merge(int *array, size_t size, size_t low, size_t cnt, char dir)
 {
-	size_t i, jump = cnt / 2;
+	size_t i, jp = cnt / 2;
 
 	if (cnt > 1)
 	{
-		for (i = low; i < low + jump; i++)
+		for (i = low; i < low + jp; i++)
 		{
-			if ((dir == 1 && array[i] > array[i + jump]) ||
-			    (dir == 0 && array[i] < array[i + jump]))
-				swap(array + i, array + i + jump, array, size);
+			if ((dir == 1 && array[i] > array[i + jp]) ||
+				(dir == 0 && array[i] < array[i + jp]))
+				swap(array + i, array + i + jp, array, size);
 		}
-		bitonic_merge(array, size, low, jump, dir);
-		bitonic_merge(array, size, low + jump, jump, dir);
+		bitonic_merge(array, size, low, jp, dir);
+		bitonic_merge(array, size, low + jp, jp, dir);
 	}
 }
 
@@ -51,22 +51,23 @@ void bitonic_merge(int *array, size_t size, int low, int cnt, int dir)
  * @cnt: Number of elements to be sorted.
  * @dir: The direction to sort in.
  */
-void bitonic_sort_rec(int *array, size_t size, int low, int cnt, int dir)
+void bitonic_sort_rec(int *array, size_t size,
+		size_t low, size_t cnt, char dir)
 {
-	size_t cut = cnt / 2;
-	char *str = (dir == 1) ? "UP" : "DOWN";
+	size_t ct = cnt / 2;
+	char *s = (dir == 1) ? "UP" : "DOWN";
 
 	if (cnt > 1)
 	{
-		printf("Merging [%u/%lu] (%s):\n", cnt, size, str);
+		printf("Merging [%lu/%lu] (%s):\n", cnt, size, s);
 		print_array(array + low, cnt);
 
-		bitonic_sort_rec(array, size, low, cut, 1);
-		bitonic_sort_rec(array, size, low + cut, cut, 0);
-		printf("Result [%u/%lu] (%s):\n", cnt, size, str);
-		print_array(array + low, cnt);
-
+		bitonic_sort_rec(array, size, low, ct, 1);
+		bitonic_sort_rec(array, size, low + ct, ct, 0);
 		bitonic_merge(array, size, low, cnt, dir);
+
+		printf("Result [%lu/%lu] (%s):\n", cnt, size, s);
+		print_array(array + low, cnt);
 	}
 }
 
