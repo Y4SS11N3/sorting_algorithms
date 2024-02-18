ea9f1f838b86fef043a974a295d412ff9b1193f2
YASSINE <contact@yassine.fun>
2024-02-18 10:42:34 -0800

    sort.h

---
+++ sort.h
@@ -0,0 +1,42 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Function prototypes for sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void lomuto_sort(int *array, ssize_t start, ssize_t end, size_t size);
+int partition(int *array, ssize_t start, ssize_t end, size_t size);
+void swap(int *a, int *b, int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* Function prototypes for printing */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
